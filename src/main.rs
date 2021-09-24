d6d129851bc35a715c99dbcea7dc2b5fdcb9958a
Richard Bradshaw <merryidleness@gmail.com>
2021-09-24 18:28:33 +0100

    Add clap. Start to parse user input.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+use clap::{App, Arg, SubCommand};
 use std::path::Path;
 use stoat::{operation::Operation, run};
 
 fn main() {
-    let op = Operation::Index(Path::new("/home/rich/projects").to_path_buf());
-    run(op).unwrap();
+    let matches = App::new("stoat")
+        .version("0.1")
+        .author("Richard Bradshaw")
+        .about("An image indexer")
+        .subcommand(
+            SubCommand::with_name("index").arg(
+                Arg::with_name("path")
+                    .short("p")
+                    .long("path")
+                    .required(true)
+                    .takes_value(true),
+            ),
+        )
+        .get_matches();
+    if let Some(matches) = matches.subcommand_matches("index") {
+        let path = Path::new(matches.value_of("path").unwrap());
+        run(Operation::Index(path.to_path_buf())).unwrap();
+    }
 }
